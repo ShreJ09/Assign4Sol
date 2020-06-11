e9dfd555990664bad19b9f2a43f65f0a1ceefc43
ShreJ <66523010+ShreJ09@users.noreply.github.com>
2020-06-11 08:57:59 +0530

    Delete goodbye.js

--- JS_assign/goodbye.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var byeName = new Object();
-    var speakWord = "Goodbye";
-    byeName.speak = function speak(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byeName = byeName;
-})(window);
