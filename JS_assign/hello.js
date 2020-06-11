8182cc1c2faa038030a55107a8b8898bfff3cd65
ShreJ <66523010+ShreJ09@users.noreply.github.com>
2020-06-11 08:58:12 +0530

    Delete hello.js

--- JS_assign/hello.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    var helloName = new Object();
-    var speakWord = "Hello";
-    helloName.speak = function(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.helloName = helloName;
-})(window);
